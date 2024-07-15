36f292ddb8e417ae696bb77a5bd51067bab3a571
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2024-07-15 19:41:37 +0900

    [feat] 레시픽 관련 엔티티(레시피, 메뉴얼, 재료) 생성 #7

---
+++ src/main/java/refooding/api/domain/recipe/Ingredient.java
@@ -0,0 +1,24 @@
+package refooding.api.domain.recipe;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import refooding.api.domain.common.BaseTimeEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Ingredient extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ingredient_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "ingredient", cascade = CascadeType.ALL) // Ingredient 엔티티 삭제시 해당 엔티티의 PK를 FK로 가지고 있는 recipeIngredient도 삭제
+    private List<RecipeIngredient> recipIngredientList = new ArrayList<>();
+
+}
