91940d8842623be1a83871d72c175833db3cd33e
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2024-07-21 22:10:14 +0900

    [feat] 레시피 조회 controller 구현 #20

--- src/main/java/refooding/api/domain/recipe/controller/RecipeController.java
+++ src/main/java/refooding/api/domain/recipe/controller/RecipeController.java
@@ -1,4 +1,90 @@
 package refooding.api.domain.recipe.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import refooding.api.domain.recipe.dto.RecipeDetailResponse;
+import refooding.api.domain.recipe.dto.RecipeResponse;
+import refooding.api.domain.recipe.service.RecipeService;
+
+import java.util.List;
+
+@Tag(name = "레시피 조회 API")
+@RestController
+@RequestMapping("/recipes")
+@RequiredArgsConstructor
 public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @GetMapping
+    @Operation(
+            summary = "전체 레시피 목록 조회",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "레시피 목록 조회 성공",
+                            content = @Content(schema = @Schema(implementation = RecipeResponse.class))
+                    )
+            }
+    )
+    public ResponseEntity<List<RecipeResponse>> getRecipes() {
+        List<RecipeResponse> response = recipeService.getRecipes();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{recipeId}")
+    @Operation(
+            summary = "레시피 상세 조회",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "레시피 상세 조회 성공",
+                            content = @Content(schema = @Schema(implementation = RecipeDetailResponse.class))
+                    )
+            }
+    )
+    public ResponseEntity<RecipeDetailResponse> getRecipeDetailById(@PathVariable Long recipeId) {
+        RecipeDetailResponse response = recipeService.getRecipeDetailById();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping()
+    @Operation(
+            summary = " 주재료 이름으로 레시피 목록 조회",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "레시피 목록 조회 성공",
+                            content = @Content(schema = @Schema(implementation = RecipeResponse.class))
+                    )
+            }
+    )
+    public ResponseEntity<RecipeDetailResponse> getRecipeDetailById(@RequestParam String ingredientName) {
+        RecipeDetailResponse response = recipeService.getRecipesByIngredientName(ingredientName);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping()
+    @Operation(
+            summary = "레시피 이름으로 레시피 목록 조회",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "레시피 목록 조회 성공",
+                            content = @Content(schema = @Schema(implementation = RecipeResponse.class))
+                    )
+            }
+    )
+    public ResponseEntity<RecipeDetailResponse> getRecipeDetailById(@RequestParam String recipeName) {
+        RecipeDetailResponse response = recipeService.getRecipesByRecipeName();
+        return ResponseEntity.ok(response);
+    }
+
 }
---
+++ src/main/java/refooding/api/domain/recipe/dto/RecipeResponse.java
@@ -0,0 +1,4 @@
+package refooding.api.domain.recipe.dto;
+
+public class RecipeResponse {
+}
