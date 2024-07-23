3786a7337f38037dfb347e4cbf054a6452628d24
younghyun.j <zerohyun.j@gmail.com>
2024-07-23 13:50:23 +0900

    [refactor] Controller Swagger 어노테이션 Interface로 분리 #23

--- src/main/java/refooding/api/domain/exchange/controller/ExchangeController.java
+++ src/main/java/refooding/api/domain/exchange/controller/ExchangeController.java
@@ -1,10 +1,5 @@
 package refooding.api.domain.exchange.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,40 +13,21 @@ import refooding.api.domain.exchange.service.RegionService;
 import java.net.URI;
 import java.util.List;
 
-@Tag(name = "식재료 교환 API")
 @RestController
 @RequestMapping("/exchanges")
 @RequiredArgsConstructor
-public class ExchangeController {
+public class ExchangeController implements ExchangeControllerOpenApi{
 
     private final ExchangeService exchangeService;
     private final RegionService regionService;
 
     @GetMapping("/{exchangeId}")
-    @Operation(
-            summary = "식재료 교환 상세 조회",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "식재료 교환 상세 조회 성공"
-                    )
-            }
-    )
     public ResponseEntity<ExchangeDetailResponse> getExchangeById(@PathVariable Long exchangeId) {
         ExchangeDetailResponse response = exchangeService.getExchangeById(exchangeId);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
-    @Operation(
-            summary = "식재료 교환글 등록",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "201",
-                            description = "식재료 교환글 등록 성공"
-                    )
-            }
-    )
     public ResponseEntity<Void> create(@RequestBody ExchangeCreateRequest request) {
         // TODO : 회원 추가
         Long exchangeId = exchangeService.create(request);
@@ -59,46 +35,18 @@ public class ExchangeController {
     }
 
     @PatchMapping("/{exchangeId}")
-    @Operation(
-            summary = "식재료 교환글 수정",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "식재료 교환글 수정 성공"
-                    )
-            }
-    )
     public ResponseEntity<Void> update(@PathVariable Long exchangeId, @RequestBody ExchangeUpdateRequest request){
         exchangeService.update(exchangeId, request);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{exchangeId}")
-    @Operation(
-            summary = "식재료 교환글 삭제",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "204",
-                            description = "식재료 교환글 삭제 성공"
-                    )
-            }
-    )
     public ResponseEntity<Void> delete(@PathVariable Long exchangeId) {
         exchangeService.delete(exchangeId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/regions")
-    @Operation(
-            summary = "식재료 교환 지역 목록 조회",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "지역 목록 조회 성공",
-                            content = @Content(schema = @Schema(implementation = RegionResponse.class))
-                    )
-            }
-    )
     public ResponseEntity<List<RegionResponse>> regions() {
         List<RegionResponse> response = regionService.getRegionsWithChildren();
         return ResponseEntity.ok(response);
