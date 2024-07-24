b2cc07c9fa33c53b2d56ae128b8d51f3c6e89e5f
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2024-07-25 00:01:48 +0900

    [feat] 테스트용 멤버 엔티티 추가 및 CRUD API 구현 #26

---
+++ src/main/java/refooding/api/domain/member/repository/MemberRepository.java
@@ -0,0 +1,20 @@
+package refooding.api.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import refooding.api.domain.member.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // 삭제되지 않은 모든 회원 조회
+    List<Member> findAllByDeletedDateIsNull();
+
+    // 삭제되지 않은 회원 ID로 조회
+    Optional<Member> findByIdAndDeletedDateIsNull(Long id);
+
+    // 삭제되지 않은 회원 중에서 이름으로 검색
+    Optional<Member> findByNameAndDeletedDateIsNull(String name);
+
+}
