fc70151e5a5c6b6c369fb0e6a950bb4289feb6cc
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2024-07-24 22:35:18 +0900

    [feat] 레시피 테스트 데이터 파싱 및 서버 올라갈 때마다 DB에 데이터 자동 삽입 기능 구현 #25

--- src/main/java/refooding/api/domain/recipe/repository/IngredientRepository.java
+++ src/main/java/refooding/api/domain/recipe/repository/IngredientRepository.java
@@ -3,5 +3,8 @@ package refooding.api.domain.recipe.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import refooding.api.domain.recipe.entity.Ingredient;
 
+import java.util.Optional;
+
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    Optional<Ingredient> findByName(String name);
 }
--- src/main/java/refooding/api/domain/recipe/service/RecipeService.java
+++ src/main/java/refooding/api/domain/recipe/service/RecipeService.java
@@ -1,5 +1,7 @@
 package refooding.api.domain.recipe.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -10,13 +12,15 @@ import refooding.api.domain.recipe.dto.ManualResponse;
 import refooding.api.domain.recipe.dto.RecipeDetailResponse;
 import refooding.api.domain.recipe.dto.RecipeIngredientResponse;
 import refooding.api.domain.recipe.dto.RecipeResponse;
-import refooding.api.domain.recipe.entity.Recipe;
+import refooding.api.domain.recipe.entity.*;
+import refooding.api.domain.recipe.repository.IngredientRepository;
 import refooding.api.domain.recipe.repository.RecipeRepository;
 
-import java.util.Collections;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @Transactional(readOnly = true)
@@ -24,6 +28,93 @@ import java.util.stream.Collectors;
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
+
+    private final IngredientRepository ingredientRepository;
+
+    @PostConstruct
+    public void init() {
+        importRecipes();
+    }
+
+    @Transactional
+    public void importRecipes() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            RecipeData recipeData = objectMapper.readValue(getClass().getClassLoader().getResourceAsStream("recipe-data.json"), RecipeData.class);
+            for (RecipeData.Recipe recipe : recipeData.getRecipes()) {
+                saveRecipe(recipe);
+            }
+            System.out.println("Recipes imported successfully");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to import recipes: " + e.getMessage());
+
+        }
+    }
+
+
+    @Transactional
+    public void saveRecipe(RecipeData.Recipe recipeJson) {
+        // Recipe 기본 정보 저장
+        Recipe recipe = Recipe.builder()
+                .name(recipeJson.getName())
+                .tip(recipeJson.getTip())
+                .mainIngredientName(recipeJson.getHashTag())
+                .mainImgSrc(recipeJson.getMainImage())
+                .build();
+
+
+        // Manual 엔티티 생성 및 저장
+        List<RecipeData.Manual> manualList = recipeJson.getManuals();
+        for (int i = 0; i < manualList.size(); i ++) {
+            RecipeData.Manual manualJson = manualList.get(i);
+            Manual manual = Manual.builder()
+                    .seq(i + 1)
+                    .content(manualJson.getContent())
+                    .imageSrc(manualJson.getImageSrc())
+                    .build();
+
+            // Manual 엔티티 저장
+            recipe.addManual(manual);
+        }
+
+        // Ingredient, RecipeIngredient 엔티티 생성 및 저장
+        String[] ingredientDetails = recipeJson.getIngredientsDetails().split(",");
+        Pattern pattern = Pattern.compile("^(.*?)(\\d+\\.?\\d*\\s?[a-zA-Z가-힣]+|[가-힣]+)$");
+        for (String ingredientDetail : ingredientDetails) {
+            Matcher matcher = pattern.matcher(ingredientDetail.trim());
+            if (matcher.find()) {
+                String ingredientName = matcher.group(1).trim();
+                String quantity = matcher.group(2).trim();
+
+                Optional<Ingredient> optionalIngredient = ingredientRepository.findByName(ingredientName);
+                Ingredient ingredient;
+                if (optionalIngredient.isPresent()) {
+                    ingredient = optionalIngredient.get();
+                } else {
+                    ingredient = Ingredient.builder()
+                            .name(ingredientName).build();
+
+                    // Ingredient 엔티티 저장
+                    ingredientRepository.save(ingredient);
+                }
+
+                RecipeIngredient recipeIngredient = RecipeIngredient.builder()
+                        .ingredient(ingredient)
+                        .quantity(quantity)
+                        .build();
+
+                // RecipeIngredient 엔티티 저장
+                recipe.addRecipeIngredient(recipeIngredient);
+            }
+        }
+
+
+        // Recipe 엔티티 저장
+        recipeRepository.save(recipe);
+
+    }
+
 
     /**
      * 레시피 목록 조회
