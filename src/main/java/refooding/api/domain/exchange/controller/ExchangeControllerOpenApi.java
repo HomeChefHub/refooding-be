26aa6668d1819484be6e2f9d9bec0fa227ec9734
younghyun.j <zerohyun.j@gmail.com>
2024-07-25 18:31:50 +0900

    [feat] Swagger 어노테이션 Interface로 분리 #23

--- src/main/java/refooding/api/domain/exchange/controller/ExchangeControllerOpenApi.java
+++ src/main/java/refooding/api/domain/exchange/controller/ExchangeControllerOpenApi.java
@@ -2,22 +2,65 @@ package refooding.api.domain.exchange.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import refooding.api.domain.exchange.dto.request.ExchangeCreateRequest;
 import refooding.api.domain.exchange.dto.request.ExchangeUpdateRequest;
 import refooding.api.domain.exchange.dto.response.ExchangeDetailResponse;
+import refooding.api.domain.exchange.dto.response.ExchangeResponse;
 import refooding.api.domain.exchange.dto.response.RegionResponse;
+import refooding.api.domain.exchange.entity.ExchangeStatus;
 
 import java.util.List;
 
 @Tag(name = "식재료 교환 API")
 public interface ExchangeControllerOpenApi {
+
+    @Operation(
+            summary = "식재료 교환 목록 조회",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "식재료 교환 목록 조회 성공"
+                    )
+            }
+    )
+    @Parameters(
+            value = {
+                    @Parameter(
+                            name = "keyword",
+                            description = "검색어"
+                    ),
+                    @Parameter(
+                            name = "regionId",
+                            description = "지역 아이디"
+                    ),
+                    @Parameter(
+                            name = "status",
+                            description = "식재료 교환 상태"
+                    ),
+                    @Parameter(
+                            name = "size",
+                            description = "요청 컨텐츠 수(0개 이상~50개 이하)"
+                    ),
+                    @Parameter(
+                            name = "lastExchangeId",
+                            description = "이전 요청 목록의 마지막 컨텐츠 아이디"
+                    ),
+            }
+    )
+    ResponseEntity<Slice<ExchangeResponse>> getExchanges(
+            String keyword,
+            Long regionId,
+            ExchangeStatus status,
+            int size,
+            Long lastExchangeId
+    );
 
     @Operation(
             summary = "식재료 교환 상세 조회",
@@ -33,7 +76,7 @@ public interface ExchangeControllerOpenApi {
             description = "게시글 아이디",
             required = true
     )
-    ResponseEntity<ExchangeDetailResponse> getExchangeById(@PathVariable Long exchangeId);
+    ResponseEntity<ExchangeDetailResponse> getExchangeById(Long exchangeId);
 
     @Operation(
             summary = "식재료 교환글 등록",
@@ -44,7 +87,7 @@ public interface ExchangeControllerOpenApi {
                     )
             }
     )
-    ResponseEntity<Void> create(@RequestBody ExchangeCreateRequest request);
+    ResponseEntity<Void> create(ExchangeCreateRequest request);
 
     @Operation(
             summary = "식재료 교환글 수정",
@@ -60,7 +103,7 @@ public interface ExchangeControllerOpenApi {
             description = "게시글 아이디",
             required = true
     )
-    ResponseEntity<Void> update(@PathVariable Long exchangeId, @RequestBody ExchangeUpdateRequest request);
+    ResponseEntity<Void> update(Long exchangeId, ExchangeUpdateRequest request);
 
     @Operation(
             summary = "식재료 교환글 삭제",
@@ -76,7 +119,7 @@ public interface ExchangeControllerOpenApi {
             description = "게시글 아이디",
             required = true
     )
-    ResponseEntity<Void> delete(@PathVariable Long exchangeId);
+    ResponseEntity<Void> delete(Long exchangeId);
 
     @Operation(
             summary = "식재료 교환 지역 목록 조회",
