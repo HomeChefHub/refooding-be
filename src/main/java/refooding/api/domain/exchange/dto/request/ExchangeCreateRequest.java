35ebadea3abceab7c668468d49fd8d8fc7a9641e
younghyun.j <zerohyun.j@gmail.com>
2024-07-29 11:21:08 +0900

    [feat] Exchange 엔티티 회원 연관관계 추가 #37

--- src/main/java/refooding/api/domain/exchange/dto/request/ExchangeCreateRequest.java
+++ src/main/java/refooding/api/domain/exchange/dto/request/ExchangeCreateRequest.java
@@ -2,13 +2,14 @@ package refooding.api.domain.exchange.dto.request;
 
 import refooding.api.domain.exchange.entity.Exchange;
 import refooding.api.domain.exchange.entity.Region;
+import refooding.api.domain.member.entity.Member;
 
 public record ExchangeCreateRequest(
         String title,
         String content,
         long regionId
 ) {
-    public Exchange toExchange(Region region) {
-        return new Exchange(title, content, region);
+    public Exchange toExchange(Region region, Member member) {
+        return new Exchange(title, content, region, member);
     }
 }
