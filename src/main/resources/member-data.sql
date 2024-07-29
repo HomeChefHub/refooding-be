074d712b7dd7827fde5bc7e160476d0cd740dfcb
younghyun.j <zerohyun.j@gmail.com>
2024-07-29 11:46:10 +0900

    [feat] 식재료 교환 repository 상세, 목록 조회 회원 데이터 조회 추가 #37

--- src/main/resources/member-data.sql
+++ src/main/resources/member-data.sql
@@ -1,2 +1,4 @@
 -- 회원
 INSERT INTO Member (name, created_date, modified_date) VALUES ('토리', '2024-07-24 14:15:49.431', '2024-07-24 14:15:49.431');
+INSERT INTO Member (name, created_date, modified_date) VALUES ('벼리', '2024-07-24 14:15:49.431', '2024-07-24 14:15:49.431');
+INSERT INTO Member (name, created_date, modified_date) VALUES ('치토스', '2024-07-24 14:15:49.431', '2024-07-24 14:15:49.431');
