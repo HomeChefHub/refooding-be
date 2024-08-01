6f1faced316fceb5c3cd3e32cbb5df3b0769f2f0
younghyun.j <zerohyun.j@gmail.com>
2024-08-01 17:18:08 +0900

    [refactor] 식재료 교환 상세조회 응답 회원 데이터 추가 #48

--- src/main/java/refooding/api/domain/exchange/dto/response/ExchangeDetailResponse.java
+++ src/main/java/refooding/api/domain/exchange/dto/response/ExchangeDetailResponse.java
@@ -9,11 +9,12 @@ public record ExchangeDetailResponse(
         Long id,
         String title,
         String content,
+        Long memberId,
+        String username,
         String region,
         String childRegion,
         ExchangeStatus status,
         LocalDateTime createDate
-        // 회원 추가
         // 이미지 추가
 ) {
     public static ExchangeDetailResponse from(Exchange exchange) {
@@ -21,6 +22,8 @@ public record ExchangeDetailResponse(
                 exchange.getId(),
                 exchange.getTitle(),
                 exchange.getContent(),
+                exchange.getMember().getId(),
+                exchange.getMember().getName(),
                 exchange.getRegion().getParent().getName(),
                 exchange.getRegion().getName(),
                 exchange.getStatus(),
