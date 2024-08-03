e3ffbd1d72358e490783146eb95b145d32896c31
younghyun.j <zerohyun.j@gmail.com>
2024-08-03 14:11:47 +0900

    [feat] Room, JoinRoom repository 추가 #38

---
+++ src/main/java/refooding/api/domain/chat/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package refooding.api.domain.chat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import refooding.api.domain.chat.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
