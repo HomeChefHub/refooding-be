4193393e2ccaeb4f3f929b38faba70b0a813a29f
younghyun.j <zerohyun.j@gmail.com>
2024-08-14 15:47:29 +0900

    [feat] 파일 업로드 예외 코드 추가 #65

--- src/main/java/refooding/api/common/exception/ExceptionCode.java
+++ src/main/java/refooding/api/common/exception/ExceptionCode.java
@@ -6,6 +6,15 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionCode {
 
+    // 400
+    FILE_NOT_PROVIDED(HttpStatus.BAD_REQUEST, "전달된 파일이 없습니다"),
+    BLANK_FILE_NAME(HttpStatus.BAD_REQUEST, "파일 이름은 비어있을 수 없습니다"),
+    INVALID_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "잘못된 파일 형식입니다"),
+    UNSUPPORTED_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "지원하지않는 파일 확장자입니다"),
+
+    // 401
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다"),
+
     // 404
     NOT_FOUND_EXCHANGE(HttpStatus.NOT_FOUND, "존재하지 않는 식재료 교환글 입니다"),
     NOT_FOUND_REGION(HttpStatus.NOT_FOUND, "존재하지 않는 지역입니다"),
@@ -14,10 +23,8 @@ public enum ExceptionCode {
     NOT_FOUND_RECIPE(HttpStatus.NOT_FOUND, "존재하지 않는 레시피입니다."),
     NOT_FOUND_MEMBER_INGREDIENT(HttpStatus.NOT_FOUND, "해당 사용자의 냉장고에 존재하지 않는 재료입니다."),
 
-    // 401
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다"),
-
     // 500
+    FILE_CONVERSION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 변환중 오류가 발생했습니다"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다"),
     TEST_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "테스트 에러입니");
 
